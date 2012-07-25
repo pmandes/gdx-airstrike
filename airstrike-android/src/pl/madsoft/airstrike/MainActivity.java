aa3e8f7db9e32bf61eced2a304098ce3f0684e45
Paweł Mandes <pawel.mandes@gmail.com>
2012-07-25 23:59:08 +0200

    config dla androida, orientacja pionowa

--- airstrike-android/src/pl/madsoft/airstrike/MainActivity.java
+++ airstrike-android/src/pl/madsoft/airstrike/MainActivity.java
@@ -6,16 +6,18 @@ import com.badlogic.gdx.backends.android.AndroidApplication;
 import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
 
 public class MainActivity extends AndroidApplication {
+	
+	
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         
         AndroidApplicationConfiguration cfg = new AndroidApplicationConfiguration();
-        cfg.useGL20 = false;
+
         cfg.useAccelerometer = false;
         cfg.useCompass = false;
         cfg.useWakelock = true;
-        cfg.useGL20 = false;
+        cfg.useGL20 = true;
         
         initialize(new AirStrikeGame(), cfg);
     }
